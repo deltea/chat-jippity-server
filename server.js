858cd11d825de60ee2f48188b307e3c987fb3ed9
deltea <thcheetah777@gmail.com>
2025-04-13 14:54:10 -0700

    error logging

--- server.js
+++ server.js
@@ -16,38 +16,57 @@ let waitingBots = [];
 io.on("connection", (socket) => {
   console.log("a user connected with id: ", socket.id);
 
-  // bot wants to find a human
-  socket.on("find-match-bot", () => {
-    console.log("bot wants to find a human, id:", socket.id);
+  socket.on("find-match", ({ type }) => {
+    console.log(type);
 
-    if (waitingHumans.length > 0) {
-      const human = waitingHumans.pop();
-      const roomId = `${human}#${socket.id}`;
-      socket.join(roomId);
-      io.sockets.sockets.get(human).join(roomId);
+    if (type === "human") {
+      // human wants to find a bot
+      console.log("human wants to find a bot, id:", socket.id);
+      if (waitingBots.length > 0) {
+        console.log("human and bot matched!");
 
-      io.to(socket.id).emit("match-found", { roomId, partnerId: human });
-      io.to(human).emit("match-found", { roomId, partnerId: socket.id });
-    } else {
-      waitingBots.push(socket.id);
+        const bot = waitingBots.pop();
+        const roomId = `${bot}#${socket.id}`;
+        socket.join(roomId);
+        io.sockets.sockets.get(bot).join(roomId);
+
+        io.to(socket.id).emit("match-found", { roomId, partnerId: bot });
+        io.to(bot).emit("match-found", { roomId, partnerId: socket.id });
+      } else {
+        waitingHumans.push(socket.id);
+      }
+    } else if (type === "bot") {
+      // bot wants to find a human
+      console.log("bot wants to find a human, id:", socket.id);
+
+      if (waitingHumans.length > 0) {
+        console.log("human and bot matched!");
+
+        const human = waitingHumans.pop();
+        const roomId = `${human}#${socket.id}`;
+        socket.join(roomId);
+        io.sockets.sockets.get(human).join(roomId);
+
+        io.to(socket.id).emit("match-found", { roomId, partnerId: human });
+        io.to(human).emit("match-found", { roomId, partnerId: socket.id });
+      } else {
+        waitingBots.push(socket.id);
+      }
     }
   });
 
-  // human wants to find a bot
-  socket.on("find-match-human", () => {
-    console.log("human wants to find a bot, id:", socket.id);
+  // human sends a message to chatgpt
+  socket.on("send-message", ({ roomId, message }) => {
+    console.log("message sent from human, roomId:", roomId);
+    socket.to(roomId).emit("receive-message", message);
+  });
 
-    if (waitingBots.length > 0) {
-      const bot = waitingBots.pop();
-      const roomId = `${bot}#${socket.id}`;
-      socket.join(roomId);
-      io.sockets.sockets.get(bot).join(roomId);
+  // chatgpt replies to the message sent by human
+  socket.on("message-reply", ({ roomId, reply }) => {
+    console.log(reply);
 
-      io.to(socket.id).emit("match-found", { roomId, partnerId: bot });
-      io.to(bot).emit("match-found", { roomId, partnerId: socket.id });
-    } else {
-      waitingHumans.push(socket.id);
-    }
+    console.log("message reply from bot, roomId:", roomId);
+    socket.to(roomId).emit("receive-reply", reply);
   });
 
   socket.on("disconnect", () => {
@@ -61,6 +80,13 @@ io.on("connection", (socket) => {
         break;
       }
     }
+  });
+
+  io.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
   });
 });
 
